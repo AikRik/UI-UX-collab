7e880b31e46dcaa3eca24f5dc40a713880a679bf
Rik Aikman <rikaikman@hotmail.com>
2017-12-20 16:44:11 +0100

    Working create eventform

--- routes/eventForm.js
+++ routes/eventForm.js
@@ -7,11 +7,34 @@ module.exports=(app,client, upload)=>{
     res.render("createEvent")
   }),
 
-  app.post("createEvent", (req,res)=>{
-    var eventname = req.body.eventname,
+  // app.post("createEvent", (req,res)=>{
+  //   var eventname = req.body.eventname,
+  //       date = req.body.date,
+  //       starttime = req.body.starttime,
+  //       endtime = req.body.endtime,
+  //       price = req.body.price,
+  //       partylink = req.body.partylink,
+  //       ticketlink = req.body.ticketlink,
+  //       venue = req.body.venue,
+  //       location = req.body.location,
+  //       city = req.body.city,
+  //       information = req.body.information,
+  //       musictype = req.body.musictype
+
+
+    
+
+  //   client.query(eventCreation, (error, result)=>{
+  //     res.render("index")
+  //   })
+  // }),
+
+	app.post('/eventForm', upload.single('eventpicture'), function (req, res, next) {
+      debugger
+      var eventname = req.body.eventname,
         date = req.body.date,
-        starttime = req.body.starttime,
-        endtime = req.body.endtime,
+        starttime = req.body.time,
+        endtime = req.body.to,
         price = req.body.price,
         partylink = req.body.partylink,
         ticketlink = req.body.ticketlink,
@@ -20,27 +43,70 @@ module.exports=(app,client, upload)=>{
         city = req.body.city,
         information = req.body.information,
         musictype = req.body.musictype
+        var picture = req.file.path 
 
-
-    var eventCreation = {
-      text: `INSERT INTO events (eventname, date, starttime, endtime, price, partylink, ticketlink, venue, location, city, information, musictype) 
-      values ('eventname','date', 'starttime','endtime','price','partylink','ticketlink','venue','location','city','information','musictype';`
-    }
-
-    client.query(eventCreation, (error, result)=>{
-      res.render("index")
-    })
-  }),
-
-	app.post('/eventForm', upload.single('eventpicture'), function (req, res, next) {
+        console.log("BLUB ", eventname,
+        date ,
+        starttime,
+        endtime,
+        price,
+        partylink,
+        ticketlink,
+        venue,
+        location,
+        city,
+        information,
+        musictype,
+        picture)
+  
+  var eventCreation = {
+        text: `INSERT INTO events 
+        (eventname, date, starttime, endtime, price, partylink, ticketlink, 
+        venue, location, city, information, musictype, eventpicture) 
+        values 
+        ('${eventname}','${date}',${starttime},${endtime},${price},'${partylink}','${ticketlink}',
+        '${venue}','${location}','${city}','${information}','${musictype}','${picture}') RETURNING *;`
+      }
    
     // match / asociate image with event
-      client.query(`INSERT INTO events (eventpicture) values ('${req.file.path}')`,(err, result)=>{
-          console.log("succes)")
-          var picture = req.file.path 
 
-         res.render("eventForm")
-      })
+      client.query(eventCreation,(err, result)=>{
+        if (err) throw err;
+
+          console.log(result)
+          
+          res.render("eventForm")
+            // client.query(`SELECT eventpicture FROM events`,(error, result2)=>{
+                   
+               // var allevents = []
+
+               //  for(var i = 0; i<result.rows.length; i++){
+               //      console.log("Supsbro", result.rows[i])
+               //      var eventResult = result.rows[i]
+               //      allevents.push(eventResult.eventpicture)
+               //  } 
+            //   console.log("allevents: ",allevents)
+            //   res.render("index", {allevents: allevents})
+            // })
+      // })
     })
 
+})
+
+
+
 }
+
+
+
+
+
+    
+
+
+
+
+
+
+
+

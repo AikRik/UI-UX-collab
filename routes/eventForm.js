2fcae251cb68002a839b20f2461815aff986b86b
Rik Aikman <rikaikman@hotmail.com>
2017-12-21 11:04:51 +0100

    working back-end create eventForm with comments

--- routes/eventForm.js
+++ routes/eventForm.js
@@ -1,37 +1,13 @@
 module.exports=(app,client, upload)=>{
 
 	app.get("/eventForm", (req,res)=>{
-    debugger
 		res.render("eventForm")
 	}),
-  app.get("/createEvent",(req,res)=>{
-    res.render("createEvent")
-  }),
-
-  // app.post("createEvent", (req,res)=>{
-  //   var eventname = req.body.eventname,
-  //       date = req.body.date,
-  //       starttime = req.body.starttime,
-  //       endtime = req.body.endtime,
-  //       price = req.body.price,
-  //       partylink = req.body.partylink,
-  //       ticketlink = req.body.ticketlink,
-  //       venue = req.body.venue,
-  //       location = req.body.location,
-  //       city = req.body.city,
-  //       information = req.body.information,
-  //       musictype = req.body.musictype
 
 
-    
-
-  //   client.query(eventCreation, (error, result)=>{
-  //     res.render("index")
-  //   })
-  // }),
-
+// uploading a single photo combined with the create event form. 
 	app.post('/eventForm', upload.single('eventpicture'), function (req, res, next) {
-      debugger
+
       var eventname = req.body.eventname,
         date = req.body.date,
         starttime = req.body.time,
@@ -43,23 +19,10 @@ module.exports=(app,client, upload)=>{
         location = req.body.location,
         city = req.body.city,
         information = req.body.info,
-        musictype = req.body.musictype
-        var picture = req.file.path 
+        musictype = req.body.musictype,
+        picture = req.file.path 
 
-        console.log("BLUB ", eventname,
-        date ,
-        starttime,
-        endtime,
-        price,
-        partylink,
-        ticketlink,
-        venue,
-        location,
-        city,
-        information,
-        musictype,
-        picture)
-  
+// the query to insert the data from the create event form into the database, and selecting the inserted data using returning *.
   var eventCreation = {
         text: `INSERT INTO events 
         (eventname, date, starttime, endtime, price, partylink, ticketlink, 
@@ -69,27 +32,27 @@ module.exports=(app,client, upload)=>{
         '${venue}','${location}','${city}','${information}','${musictype}','${picture}') RETURNING *;`
       }
    
-    // match / asociate image with event
 
       client.query(eventCreation,(err, result)=>{
         if (err) throw err;
 
           console.log(result)
-          
-          res.render("eventForm")
-            // client.query(`SELECT eventpicture FROM events`,(error, result2)=>{
-                   
-               // var allevents = []
+          // rendering the indexpage with all the selected data from the insert query.
+          res.render("index",{eventname:eventname,
+                              date: date,
+                              starttime: starttime,
+                              endtime: endtime,
+                              price: price,
+                              partylink: partylink,
+                              ticketlink: ticketlink,
+                              venue: venue,
+                              location: location,
+                              city: city,
+                              information: information,
+                              musictype: musictype,
+                              eventpicture: picture})
 
-               //  for(var i = 0; i<result.rows.length; i++){
-               //      console.log("Supsbro", result.rows[i])
-               //      var eventResult = result.rows[i]
-               //      allevents.push(eventResult.eventpicture)
-               //  } 
-            //   console.log("allevents: ",allevents)
-            //   res.render("index", {allevents: allevents})
-            // })
-      // })
+
     })
 
 })

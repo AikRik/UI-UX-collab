1750a61326b1fa767cef04e56b9981c9930882d4
Rik Aikman <rikaikman@hotmail.com>
2018-01-03 13:06:58 +0100

    minor adjustments to time display and urls

--- routes/eventForm.js
+++ routes/eventForm.js
@@ -20,41 +20,34 @@ module.exports = (app, client, upload) => {
               information = req.body.info,
               musictype = req.body.musictype,
               picture = req.file.path
+              console.log("STARTTIME", starttime)
 
+              // function escape(time){
+              //   var a = time
+              
+              //   var slice = [a.slice(0,1), a.slice(3)].join("/");
+              //   return slice
+              // }
+              // console.log(escape(starttime))
+// Query does not work with backspaces. 
           var eventCreation = {
-              text: `INSERT INTO events(eventname, date, starttime, endtime, price, partylink, ticketlink,venue,location, city, information, musictype, eventpicture) values ('${eventname}','${date}',${starttime},${endtime},${price},'${partylink}','${ticketlink}', '${venue}','${location}','${city}','${information}','${musictype}','${picture}') RETURNING *;`
+              text: `INSERT INTO events(eventname, date, starttime, endtime, price, partylink, ticketlink,venue,location, city, information, musictype, eventpicture) values ('${eventname}','${date}','${starttime}', '${endtime}', ${price},'${partylink}','${ticketlink}', '${venue}','${location}','${city}','${information}','${musictype}','${picture}') RETURNING *;`
     }
-    debugger
+
 // the query to insert the data from the create event form into the database, and selecting the inserted data using returning *.     
     client.query(eventCreation, (err, result)=>{
       if (err) console.log("Error",err);
-          debugger
-          client.query(`SELECT (eventname, date, starttime, endtime, price, partylink, ticketlink, 
-                        venue, location, city, information, musictype, eventpicture) 
-                        FROM events`, (err, result)=>{
+
+          client.query(`SELECT (eventname, date, starttime, endtime, price, partylink, ticketlink, venue, location, city, information, musictype, eventpicture) FROM events`, (err, result)=>{
                       var allevents = []
                       if(err){throw err}
-                        debugger
                       for(var i = 0; i<result.rows.length; i++){
                       var eventResult = result.rows[i]
 
                   allevents.push(eventResult)
               }
 
-              res.render("eventPage", {
-                  eventname: eventname,
-                  date: date,
-                  starttime: starttime,
-                  endtime: endtime,
-                  price: price,
-                  partylink: partylink,
-                  ticketlink: ticketlink,
-                  venue: venue,
-                  location: location,
-                  city: city,
-                  information: information,
-                  musictype: musictype,
-                  eventpicture: picture
+              res.render("eventPage", {eventname: eventname, date: date, starttime: starttime, endtime: endtime, price: price,  partylink: partylink, ticketlink: ticketlink, venue: venue, location: location, city: city, information: information, musictype: musictype, eventpicture: picture
               })
           })
 
